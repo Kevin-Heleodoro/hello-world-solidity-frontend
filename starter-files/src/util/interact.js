f4ffd470b75296be245abd8f2f8769b733437b51
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-04-19 17:09:49 -0500

    loading current message from contract

--- starter-files/src/util/interact.js
+++ starter-files/src/util/interact.js
@@ -1,17 +1,23 @@
-//export const helloWorldContract;
+require('dotenv').config({ path: __dirname + '/.env' });
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const contractAddress = '0x6E51A5e025304EFE6cA23c41Ff313105F79e0289';
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+const web3 = createAlchemyWeb3(alchemyKey);
 
-export const loadCurrentMessage = async () => { 
-  
+const contractABI = require('../contract-abi.json');
+
+export const helloWorldContract = new web3.eth.Contract(
+	contractABI,
+	contractAddress
+);
+
+export const loadCurrentMessage = async () => {
+	const message = await helloWorldContract.methods.message().call();
+	return message;
 };
 
-export const connectWallet = async () => {
-  
-};
+// export const connectWallet = async () => {};
 
-export const getCurrentWalletConnected = async () => {
-  
-};
+// export const getCurrentWalletConnected = async () => {};
 
-export const updateMessage = async (address, message) => {
-  
-};
+// export const updateMessage = async (address, message) => {};
